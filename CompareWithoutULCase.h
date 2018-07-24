0858c38a256fa0080d97eb1abd7b61413e49d1ea
i@foolsparadise.cn <23549415+foolsparadise@users.noreply.github.com>
2018-07-24 19:03:03 +0800

    Update CompareWithoutULCase.h

--- CompareWithoutULCase.h
+++ CompareWithoutULCase.h
@@ -9,6 +9,11 @@
 #ifndef CompareWithoutULCase_h
 #define CompareWithoutULCase_h
 
+/*  
+usage:  
+see CompareWithoutULCase.cpp file main() function
+使用方法:参考cpp文件中的main函数里代码
+*/  
 int CompareWithoutULCase(char * str1, char * str2);
 
 #endif /* CompareWithoutULCase_h */
